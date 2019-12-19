735ee3f85657c2fefca84996d34712fa2439c30b
yutsukushi <ykushibi@gmail.com>
2019-12-19 16:36:48 -0500

    react commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 // import logo from './logo.svg';
 import SortList from './pages/epartslist'
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <SortList/>
-    </div>
-  );
+    <Router>
+      <Route exact path="/" component={ SortList }/>
+    </Router>
+  )
 }
 
 export default App;
