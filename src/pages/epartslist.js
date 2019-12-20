e5a61994fee8f6d7a681491d6a1dcaf14be36a86
yutsukushi <ykushibi@gmail.com>
2019-12-20 00:13:44 -0500

    cleaning up files, adding/removing comments for clarity and works in progress

--- src/pages/epartslist.js
+++ src/pages/epartslist.js
@@ -24,26 +24,20 @@ class SortList extends Component {
             return res.json();
         }).then(data => {
             this.setState({Products: data});
-            console.log("state", this.state.Products[0].Name);
         })
     }
 
+    // View List button event handler that greets user in console.log
     handleView() {
         console.log("hello")
     }
 
+    // function displaying four product entries per card to minimize awkward card sizes
     // displayFour() {
     //     let epartsProduct = this.state.Products;
     //     for (let i = 0; i < 3; i++) {
     //         return <p key={epartsProduct.ProductID}>{epartsProduct.ProductNumber}</p>
     //     }
-    // }
-
-    // convertDate() {
-    //     const moment = require('moment');
-
-    //     let unixDate = this.state.Products.CreatedDate;
-    //     let date = moment(unixDate).format('L');
     // }
 
     sortFilter(e) {
@@ -55,13 +49,16 @@ class SortList extends Component {
         productsArr.sort((a,b) => {
             let nameOne = a.Name.toLowerCase();
            let nameTwo = b.Name.toLowerCase();
+        // if form inputs === "ascend" or "descend", sort renders appropriately
            if (value === "ascend") {
                 return (nameOne < nameTwo) ? -1 : (nameOne > nameTwo) ? 1 : 0;
            } else if (value === "descend") {
                return (nameOne < nameTwo) ? 1 : (nameOne > nameTwo) ? -1 : 0;
+           } else {
+               return productsArr;
            }
         })
-
+        // storing my hard work somewhere to be remembered
         this.setState({sortedList: productsArr })
     }
     
@@ -78,37 +75,35 @@ class SortList extends Component {
                 </div>
 
                 <div className="row">
-                        <List>
-                            {this.state.Products.map(card => (
-                                    <ListItem
-                                    key={card.ListID}>
-                                        <Card
-                                        name={card.Name}
-                                        createdate={moment(card.CreatedDate).format("MM/DD/YYYY")}
-                                        products={card.Products.map(epartsProduct => (
-                                        <p key={epartsProduct.ProductID}>{epartsProduct.ProductNumber}
-                                        </p> 
-                                        ))}
-                                        total={card.Total}
-                                        items={card.TotalItems}
-                                        Button={() => (
-                                            <div className="button">
-                                                <button
-                                                    data-id={card.ListID}
-                                                    onClick={(event) => this.handleView(event)}
-                                                >
-                                                    View List
-                                                </button>
-                                            </div>
-                                        )} 
-                                        />
-                                    </ListItem>
-                                
-                            ))}
-                            
-                        </List>
-                        </div>
-                    </div>
+                    <List>
+                        {this.state.Products.map(card => (
+                            <ListItem
+                            key={card.ListID}>
+                                <Card
+                                name={card.Name}
+                                createdate={moment(card.CreatedDate).format("MM/DD/YYYY")}
+                                products={card.Products.map(epartsProduct => (
+                                <p key={epartsProduct.ProductID}>{epartsProduct.ProductNumber}
+                                </p> 
+                                ))}
+                                total={card.Total}
+                                items={card.TotalItems}
+                                Button={() => (
+                                    <div className="button">
+                                        <button
+                                            data-id={card.ListID}
+                                            onClick={(event) => this.handleView(event)}
+                                        >
+                                            View List
+                                        </button>
+                                    </div>
+                                )} 
+                                />
+                            </ListItem>
+                        ))}
+                    </List>
+                </div>
+            </div>
         )
     }
 }
