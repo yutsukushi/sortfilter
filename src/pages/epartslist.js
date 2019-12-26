d0fe38092db4bb133a6b732c1f3b4226683e89de
yutsukushi <ykushibi@gmail.com>
2019-12-26 13:21:58 -0500

    cleaned up/commented out nonfunctional code and cards have products rendered

--- src/pages/epartslist.js
+++ src/pages/epartslist.js
@@ -11,10 +11,11 @@ class SortList extends Component {
         super();
         this.state = {
             Products: [],
-            sortedList: []
+            sortedList: [],
+            // displayFive: []
         }
         this.sortFilter = this.sortFilter.bind(this);
-        // this.displayFour = this.displayFour.bind(this);
+        this.displayFive = this.displayFive.bind(this);
     }
 
     // fetch data function
@@ -33,13 +34,52 @@ class SortList extends Component {
         console.log("hello")
     }
 
-    // function displaying four product entries per card to minimize awkward card sizes
-    // displayFour() {
-    //     let epartsProduct = this.state.Products;
-    //     for (let i = 0; i < 3; i++) {
-    //         return <p key={epartsProduct.ProductID}>{epartsProduct.ProductNumber}</p>
-    //     }
-    // }
+    // function displaying five product entries per card to minimize awkward card sizes
+    
+        // iterate through each object
+        // with each object, loop through products
+        // and list every product in object/render on DOM
+        // console.log("products", products)
+
+        displayFive() {
+            let products = this.state.Products;
+            products.forEach(function(products) {
+                
+                for (let i = 0, len = Math.min(4, products.Products.length); i < len; i++) {
+                // https://stackoverflow.com/questions/43295840/javascript-how-to-start-foreach-loop-at-some-index
+
+                // console.log("products.length",products.Products.length)
+                // console.log("products.linenumber", products.Products[i].LineNumber)
+                // console.log("products.productNumber", products.Products[i].ProductNumber)
+                    return (
+                        <p key={products.Products[i].LineNumber}>{products.Products[i].ProductNumber}</p>
+                    )
+                }
+            })
+            // -------------------------------------------------
+            // return (
+            //     <ul>
+            //         {card.Products.map(function(products) {
+            //             return <li key={products.LineNumber}>{products.ProductNumber}></li>
+            //         })}
+            //     </ul>
+            // )
+            // ------------------------------------------------
+            // card.Products.forEach(function (products) {
+                // console.log("products", products);
+                    // return (
+                    // <ul></ul><p key={products.LineNumber}>{products.ProductNumber}</p>)
+                    // console.log("for each product", products);
+               
+                // if (products === undefined) {
+                //     return <p></p>
+                // } else {
+                // // console.log("displayFive", fiveProducts[i].Products[i].ProductID);
+                //     return <p key={products.ProductID}>{products}</p>
+                // }
+            // }) 
+        // })
+        }     
 
     sortFilter(e) {
         // Credit to: https://stackoverflow.com/questions/6712034/sort-array-by-firstname-alphabetically-in-javascript
@@ -52,9 +92,23 @@ class SortList extends Component {
            let nameTwo = b.Name.toLowerCase();
         // if form inputs === "ascend" or "descend", sort renders appropriately
            if (value === "ascend") {
-                return (nameOne < nameTwo) ? -1 : (nameOne > nameTwo) ? 1 : 0;
+               if (nameOne < nameTwo) {
+                   return -1
+               } else if (nameOne > nameTwo) {
+                   return 1
+               } else {
+                   return 0
+               }
+                // return (nameOne < nameTwo) ? -1 : (nameOne > nameTwo) ? 1 : 0;
            } else if (value === "descend") {
-               return (nameOne < nameTwo) ? 1 : (nameOne > nameTwo) ? -1 : 0;
+               if (nameOne < nameTwo) {
+                   return 1
+               } else if (nameOne > nameTwo) {
+                   return -1
+               } else {
+                   return 0
+               }
+            //    return (nameOne < nameTwo) ? 1 : (nameOne > nameTwo) ? -1 : 0;
            } else {
                return productsArr;
            }
@@ -83,10 +137,20 @@ class SortList extends Component {
                                     <Card
                                     name={card.Name}
                                     createdate={moment(card.CreatedDate).format("MM/DD/YYYY")}
-                                    products={card.Products.map(epartsProduct => (
-                                    <p key={epartsProduct.LineNumber}>{epartsProduct.ProductNumber}
-                                    </p> 
-                                    ))}
+                                    products={card.Products.slice(0,5).map(
+                                            epartsProduct => {
+                                            return <p key={epartsProduct.LineNumber}>{epartsProduct.ProductNumber}</p>
+                                        })
+                                    }
+                                    // {this.displayFive()}
+
+                                    // function(epartsProduct) {
+                                    // for (let i = 0, len = Math.min(4, epartsProduct.length); i < len; i++) {
+                                        
+                                    //     }
+                                    //     return <p></p> 
+                                    // })
+                                    
                                     total={card.Total}
                                     items={card.TotalItems}
                                     Button={() => (
